f3ce0537561cf830699c3d77fd9d38845fc7ad3a
Melissa <42444622+masand118@users.noreply.github.com>
2018-09-02 13:05:39 -0400

    cleaned up some code, added some graphing and tables

--- logistic_regression/HW 1 code_melissa.R
+++ logistic_regression/HW 1 code_melissa.R
@@ -1,74 +1,131 @@
 library(tidyverse)
 library(haven)
 library(broom)
-#########
-# read the sas dataset into an R dataframe
+library(MASS)
+library(visreg)
+library(brglm)
 
-train <- read_sas("C:\\Users\\Melissa Sandahl\\OneDrive\\Documents\\School\\MSA courses\\AA502\\Logistic Regression\\data\\insurance_t.sas7bdat")
+
+# read the sas dataset into an R dataframe
+train <- read_sas("C:\\Users\\Melissa Sandahl\\OneDrive\\Documents\\School\\MSA courses\\AA502\\Logistic regression\\data\\insurance_t.sas7bdat")
 
 # look at the structure of the dataframe to see the class of each variable
 str(train)
+p_summary <- psych::describe(train)
+h_summary <- Hmisc::describe(train)
+summary(train)
 
 # create a character vector which counts all the missing values for variables
 missing <- train %>% 
-  select_if(function(x) any(is.na(x))) %>% 
+  dplyr::select_if(function(x) any(is.na(x))) %>% 
   summarise_all(funs(sum(is.na(.))))
 
 # create a new dataframe without the variables with missing values
 train_no_NAs <- train %>% 
-  select(-!! names(missing))
+  dplyr::select(-!! names(missing))
 
 # create a dataframe which creates the 5 number summary of the variables
 train_summary <- tidy(summary(train_no_NAs))
 
-# create histograms for all the variables except branch and res
+#Histograms of selected variables without missing values
 ggplot(gather(train_no_NAs, key, value, -c(BRANCH, RES)), aes(value)) +
-  geom_histogram() + facet_wrap(~ key, scales = "free")
+  geom_histogram(color = "blue") +
+  facet_wrap(~ key, scales = "free") + theme_bw() +
+  labs(y = "Frequncy of Each Variable", x = "Multiple Variables Defined Above", title = "Histograms for the Variables with no Missing Observations")
 
-# reduced dataframe based on the histograms. Deleted these variables due to 
-# minimal variation in the data
+# filtering binary variables where one count is less than 1000
+check <- train_no_NAs %>%
+  # select columns which only have 2 distint values (ie 0 and 1)
+  select_if(~ n_distinct(.) ==2) %>% 
+  # find sum of those columns which should be count of 1's
+  map_dbl(~ sum(.))
+
+# find the names of the columns above that are under 100
+low_count_var <- names(which(check < 1000, useNames = TRUE))
+low_count_var
+
 train_reduced <- train_no_NAs %>% 
-  dplyr::select(-c(CASHBK, NSF, NSFAMT,
-           CD, CDBAL, IRA, IRABAL, LOC, LOCBAL, ILS, 
-            ILSBAL, MM, MMBAL, MMCRED, MTG, MTGBAL, SDB, MOVED, 
-            INAREA))
+  # drop the columns from above
+  dplyr::select(-c(!! low_count_var)) 
+
+# checking the histograms of the new reduced df
+ggplot(gather(train_reduced, key, value, -c(BRANCH, RES)), aes(value)) +
+  geom_histogram(color = "blue") +
+  facet_wrap(~ key, scales = "free") +
+  theme_bw()
+
+
 
 # Histograms of the selected variables
 ggplot(gather(train_reduced, key, value, -c(BRANCH, RES)), aes(value)) +
   geom_histogram() +  facet_wrap(~ key, scales = "free")
 
 
-# Possible libraries needed for logistic regression
-library(MASS)
-library(visreg)
-library(brglm)
+
 
 #Run logistic regression with all train_reduced variables included
-fit <- glm(INS ~ DDA + DDABAL + DEP + DEPAMT + CHECKS + DIRDEP + TELLER 
-                       + SAV + SAVBAL + ATM + ATMAMT + RES + BRANCH,
+fit <- glm(INS ~ DDA + DDABAL + DEP + DEPAMT + CHECKS + 
+             DIRDEP + TELLER + SAV + SAVBAL + ATM + 
+             ATMAMT + RES + BRANCH,
            data = train_reduced, family = binomial(link = "logit"))
 summary(fit)
 exp(confint(fit))
 
 #Remove variables with pvalues above 0.05 in "fit". Rerun logistic regression.
 # Removed were: DEPTAMT, DIRDEP, RES, BRANCH
+set.seed(1234)
 fit2 <- glm(INS ~ DDA + DDABAL + DEP + CHECKS + TELLER 
-           + SAV + SAVBAL + ATM + ATMAMT + BRANCH,
-           data = train_reduced, family = binomial(link = "logit"))
+            + SAV + SAVBAL + ATM + ATMAMT + BRANCH,
+            data = train_reduced, family = binomial(link = "logit"))
 summary(fit2)
+sort(abs(fit2$coefficients))
 exp(confint(fit2))
+diff(exp(fit2$coefficients))
+exp(fit2$coefficients)
 
-# All variables in "fit2" model are significant. These are:
-  # ATM
-  # DEP: checking deposits
-  # ATMAMT: ATM withdrawal amount
-  # SAVBAL: saving balance
-  # SAV: saving account
-  # Teller: teller visits
-  # Checks: # checks
-  # DDABAL: checking balance
-  # DDA: checking account
+# Comparing the AIC for removal of columns that might seem redundant
+fit3 <- glm(INS ~ DDABAL + DEP + CHECKS + TELLER 
+            + SAVBAL + ATMAMT + BRANCH,
+            data = train_reduced, family = binomial(link = "logit"))
+summary(fit3)
+
+# adding the credit score back in because it only had 195 missing obs
+fit4 <- glm(INS ~ DDABAL + DEP + CHECKS + TELLER 
+            + SAVBAL + ATMAMT + BRANCH + CRSCORE,
+            data = cbind(train_reduced, CRSCORE = train$CRSCORE), family = binomial(link = "logit"))
+summary(fit4)
+
+
+# Checking distributions of variables with missing information
+train_missing <- train %>% 
+  dplyr::select(!! names(missing))
+
+
+#Plot of variables with missing values
+ggplot(gather(train_missing, key, value), aes(value)) +
+  geom_histogram(color = "blue") +facet_wrap(~ key, scales = "free") + theme_bw() +
+  labs(y = "Frequncy of Each Variable", x = "Multiple Variables Defined Above", title = "Histograms for the Variables with Missing Observations")
 
 
 
 
+#Dataset with only the variables in the model
+train_model_vars <- train %>%
+  dplyr::select(INS, DDABAL, DEP, CHECKS, TELLER, SAVBAL, ATMAMT, BRANCH, CRSCORE)
+
+#Removing Branch from the list in order to plot histograms
+train_model_no_branch <- train %>%
+  dplyr::select(INS, DDABAL, DEP, CHECKS, TELLER, SAVBAL, ATMAMT, CRSCORE)
+
+summary(train_for_stats)
+
+#Plotting histograms of the variables selected for the model, not including Branch.
+ggplot(gather(train_for_stats, key, value), aes(value)) + geom_histogram(color = "blue") +
+  facet_wrap(~ key, scales = "free") + theme_bw() +
+  labs(y = "Frequncy of Each Variable", x = "Multiple Variables Defined Above", title = "Histograms for Model Variables")
+
+#Count the number of customers that purchased the insurance product and those that didn't.
+counts <- train %>%
+  count(INS) %>%
+  mutate(prop = prop.table(n))
+counts
