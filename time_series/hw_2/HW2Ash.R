0b455b7213d79399f8c9a9addfe427f3b90da7ef
ashwinb6 <ashwinb6@github.com>
2018-09-06 09:24:35 -0400

    trying on my own

--- time_series/hw_2/HW2Ash.R
+++ time_series/hw_2/HW2Ash.R
@@ -1,28 +1,54 @@
 library(readxl)
 library(lubridate)
 library(tidyverse)
+library(magrittr)
+library(forecast)
+library(fma)
+library(expsmooth)
+library(lmtest)
+library(zoo)
+library(seasonal)
+library(ggplot2)
 setwd("time_series/hw_2")
-excel_sheets("../F-179.xlsx")
-# rain <- read_excel("F-179.xlsx", sheet = "Rain")
-# tide <- read_excel("F-179.xlsx", sheet = "Tide")
 
 # read only the well sheet from excel
 well <- read_excel("../F-179.xlsx", sheet = "Well")
 
-#well <- read_excel("F-179.xlsx", sheet = "Well", col_types = c("date", "date", "skip", "skip", "skip", "numeric"))
-# summary(rain)
-# summary(tide)
+well_2 <- well %>% 
+  mutate(Corrected = Corrected + 1) %>% 
+  group_by(date = as.yearmon(date)) %>% 
+  summarise(avg = mean(Corrected))
 
-# look at 5 number summary of dataframe
-summary(well)
+# well_3 <- well %>% 
+#   group_by(year(date), month(date)) %>% 
+#   summarise(avg = mean(Corrected)) %>% 
+#   unite(date, 1, 2, sep = "-") %>% 
+#   mutate(date = zoo::as.yearmon(date))
 
-# histogram plot of well
-hist(well$Corrected)
+# created testing set with last 6 months of data
+train <- well_2 %>% 
+  filter(date <= '2017-12')
+test <- anti_join(well_2, train, by = "date")
 
 
-# Question 1 ####
-well %>% 
-  group_by(year(date), month(date)) %>% 
-  summarise(avg = mean(Corrected)) %>% 
-  unite(date, 1, 2, sep = "-") %>% 
-  mutate(date = zoo::as.yearmon(date))
+# make the timeseries object
+well_ts <- ts(train$avg, start = c(2007,10), frequency = 12)
+
+sesWell <- ses(well_ts, initial = "optimal", h = 6)
+summary(sesWell)
+
+plot(sesWell, main = "Well depth Simple SES Forecast", xlab = "Date", ylab = "Depth (ft)")
+abline(v = 2018, col = "red", lty = "dashed")
+round(accuracy(sesWell,2))
+
+    
+#Linear Exp Smoothing Model
+
+lesWell <- holt(well_ts, initial = "optimal", h=6)
+summary(lesWell)
+
+plot(lesWell, main = "Well Depth with Linear ESM Forecast", xlab = "Date", ylab = "Well depth (ft)")
+abline(v = 2018, col = "red", lty = "dashed")
+
+autoplot(lesWell)+
+  autolayer(fitted(lesWell),series="Fitted")+ylab("Well Depth with Holt ESM Forecast")
