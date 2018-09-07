60c0cff30e11a893a93dc82a059061611f51ae3f
ashwinb6 <ashwinb6@github.com>
2018-09-06 21:21:55 -0400

    trying more

--- time_series/hw_2/HW2Ash.R
+++ time_series/hw_2/HW2Ash.R
@@ -19,6 +19,7 @@ well_2 <- well %>%
   group_by(date = as.yearmon(date)) %>% 
   summarise(avg = mean(Corrected))
 
+view(well_2)
 # well_3 <- well %>% 
 #   group_by(year(date), month(date)) %>% 
 #   summarise(avg = mean(Corrected)) %>% 
@@ -27,23 +28,39 @@ well_2 <- well %>%
 
 # created testing set with last 6 months of data
 train <- well_2 %>% 
-  filter(date <= '2017-12')
-test <- anti_join(well_2, train, by = "date")
+  filter(date < 'Jan 2018')
+test <- well_2 %>%
+  filter(date >= 'Jan 2018')
 
 
 # make the timeseries object
 well_ts <- ts(train$avg, start = c(2007,10), frequency = 12)
 
+#STL Decomposition
+model <- stl(well_ts,s.window=7)
+plot(model)
+
+plot(well_ts,col="dark gray",
+     main="Well Depth - Trend/Cycle",
+     xlab="Time", ylab = "Depth (ft.)", lwd=2)
+lines(model$time.series[,2],col="red",lwd=2)
+
+#Seasonally Adj.
+seas = well_ts - model$time.series[,1]
+plot(well_ts, col='gray', main="Well Depth - Seasonally Adj.", xlab="time",ylab="depth",lwd=2)
+lines(seas,col="red",lwd=2)
+
+#Simple Exp Smoothing Model
 sesWell <- ses(well_ts, initial = "optimal", h = 6)
 summary(sesWell)
 
 plot(sesWell, main = "Well depth Simple SES Forecast", xlab = "Date", ylab = "Depth (ft)")
 abline(v = 2018, col = "red", lty = "dashed")
-round(accuracy(sesWell,2))
+summary(sesWell)
+
 
     
 #Linear Exp Smoothing Model
-
 lesWell <- holt(well_ts, initial = "optimal", h=6)
 summary(lesWell)
 
@@ -52,3 +69,5 @@ abline(v = 2018, col = "red", lty = "dashed")
 
 autoplot(lesWell)+
   autolayer(fitted(lesWell),series="Fitted")+ylab("Well Depth with Holt ESM Forecast")
+
+
